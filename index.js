2023dfd5116f7731e6ea10f0ed97ae3c69024c6e
Randall Degges <r@rdegges.com>
2015-12-18 20:03:55 -0600

    Various bits of cleanup.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
+'use strict';
+
 var express = require('express');
 var stormpath = require('express-stormpath');
-var app = express();
 
-app.set('port', process.env.PORT || 5000);
+var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-// app.use(stormpath.init(app, {
-//   website: true 
-// }));
 
 app.use(stormpath.init(app, {
   web: {
@@ -32,9 +29,7 @@ app.get('/', stormpath.loginRequired, function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('new config');
-  console.log('Node app is running on port', app.get('port'));
+app.on('stormpath.ready', function() {
+  app.listen(app.get('port'));
+  console.log('Node app is running on port:', app.get('port'));
 });
-
-
